6fbaeaaf0bfca4a2a96c76f1d201e91e26cad982
HuyenTram <tramb1900239@student.ctu.edu.vn>
2022-10-29 18:05:14 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- myshop/lib/ui/products/edit_product_screen.dart
+++ myshop/lib/ui/products/edit_product_screen.dart
@@ -82,11 +82,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
     try {
       final productsManager = context.read<ProductsManager>();
-      //  print(_editedProduct.title);
       if (_editedProduct.id != null) {
-        productsManager.updateProduct(_editedProduct);
+        await productsManager.updateProduct(_editedProduct);
       } else {
-        productsManager.addProduct(_editedProduct);
+        await productsManager.addProduct(_editedProduct);
       }
     } catch (error) {
       await showErrorDialog(context, 'Something went wrong.');
